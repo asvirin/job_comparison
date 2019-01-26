b49b5138fa519d080fc9be58981796a3d8b2ca7c
asvirin <45921969+asvirin@users.noreply.github.com>
2019-01-26 23:51:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,78 @@
-
 import requests
 import os
 from terminaltables import AsciiTable
 from dotenv import load_dotenv
 
-
-def predict_rub_salary_for_headhunter(vacancy_salary):
-    if vacancy_salary == None or vacancy_salary['currency'] != 'RUR':
-        return None
-    elif (vacancy_salary['from'] and vacancy_salary['to']) != None:
-        return (vacancy_salary['from'] + vacancy_salary['to']) / 2
-    elif vacancy_salary['from'] != None:
-        return vacancy_salary['from'] * 1.2
-    else:
-        return vacancy_salary['to'] * 0.8
-
-
-def predict_rub_salary_for_superjob(salary_from, salary_to):
-    if salary_from == 0 and salary_to == 0:
-        return None
-    elif salary_from == 0:
+def get_avarage_salary(salary_from, salary_to):
+    if salary_from == 0 or salary_from is None:
         return salary_to * 0.8
-    elif salary_to == 0:
+    elif salary_to == 0 or salary_to is None:
         return salary_from * 1.2
     else:
         return (salary_from + salary_to) / 2
 
 
-def get_vacancies_statistics_headhunter(languages_list, head_table):
-    vacancies_statistics_headhunter_list = head_table
+def predict_rub_salary_for_headhunter(vacancy_salary):
+    if vacancy_salary is None or vacancy_salary['currency'] != 'RUR':
+        return None
+    else:
+        return get_avarage_salary(vacancy_salary['from'], vacancy_salary['to'])
 
+
+def predict_rub_salary_for_superjob(salary_from, salary_to):
+    if (salary_from == 0 and salary_to == 0) or (salary_from is None and salary_to is None):
+        return None
+    else:
+        return get_avarage_salary(salary_from, salary_to)
+    
+def get_table_for_print(source, vacancies_list):
+    title_table = '{} Moscow'.format(source)
+    table = AsciiTable(vacancies_list, title_table)
+    return table.table
+
+def get_language_statistics(language, source, vacancies_all_pages):
+    vacancies_processed = 0
+    salary_language_sum = 0
+    
+    for vacancy in vacancies_all_pages:
+        if source == 'Headhunter':
+            predict_salary = \
+                predict_rub_salary_for_headhunter(vacancy['salary'])
+        else:
+            predict_salary = \
+                    predict_rub_salary_for_superjob(vacancy['payment_from'
+                        ], vacancy['payment_to'])
+        if predict_salary is not None:
+            vacancies_processed += 1
+            salary_language_sum += predict_salary      
+
+    vacancies_found = len(vacancies_all_pages)
+        
+    if vacancies_processed == 0:
+        return [language, vacancies_found, 0, 0]
+            
+    else:
+        return [language, vacancies_found, vacancies_processed,
+                    int(salary_language_sum / vacancies_processed)]
+
+
+def get_vacancies_statistics_Headhunter(source, languages_list, head_table):
     url_api_headhunter = 'https://api.hh.ru/vacancies/'
+    specialization = '1.221'
+    location = '1'
+    period_search = '30'
+    result_per_page = '20'
+    max_result = '2000'
+    
+    vacancies_statistics_list = head_table
 
     for language in languages_list:
         page = 0
-        vacancies_processed = 0
-        salary_language_sum = 0
-
         payload = {
-            'specialization': 1.221,
-            'area': 1,
-            'period': 30,
+            'specialization': specialization,
+            'area': location,
+            'period': period_search,
+            'per_page': result_per_page,
             'text': language,
             }
         response = requests.get(url_api_headhunter, params=payload)
@@ -48,93 +80,64 @@ def get_vacancies_statistics_headhunter(languages_list, head_table):
 
         vacancies_all_pages = []
 
-        while vacancies['found'] > len(vacancies_all_pages) <= 1980:
+        while vacancies['found'] > len(vacancies_all_pages) <= (int(max_result)-int(result_per_page)):
             payload = {
-                'specialization': 1.221,
-                'area': 1,
-                'period': 30,
+                'specialization': specialization,
+                'area': location,
+                'period': period_search,
                 'page': page,
+                'per_page': result_per_page,
                 'text': language,
                 }
             response = requests.get(url_api_headhunter, params=payload)
             vacancies = response.json()
             vacancies_all_pages += vacancies['items']
             page += 1
+            
+        if len(vacancies_all_pages) == 0:
+            vacancies_statistics_list.append([language, 0, 0, 0])
+        else:
+            vacancies_statistics_list.append(get_language_statistics(language, source, vacancies_all_pages))
+            
+    table_for_print = get_table_for_print(source, vacancies_statistics_list)
 
-        for vacancy in vacancies_all_pages:
-            predict_salary = \
-                predict_rub_salary_for_headhunter(vacancy['salary'])
-            if predict_salary != None:
-                vacancies_processed += 1
-                salary_language_sum += predict_salary
-
-        vacancies_found = len(vacancies_all_pages)
-        try:
-            vacancies_statistics_headhunter_list.append([language,
-                    vacancies_found, vacancies_processed,
-                    int(salary_language_sum / vacancies_processed)])
-        except ZeroDivisionError:
-
-            vacancies_statistics_headhunter_list.append([language,
-                    vacancies_found, 0, 0])
-
-    return vacancies_statistics_headhunter_list
+    return table_for_print
 
 
-def get_vacancies_statistics_superjob(languages_list, head_table):
-    vacancies_statistics_superjob_list = head_table
-
+def get_vacancies_statistics_SuperJob(source, languages_list, head_table):
     url_api_superjob = 'https://api.superjob.ru/2.0/vacancies/'
     api_key_superjob = os.getenv("api_key_superjob")
     headers = {'X-Api-App-Id': api_key_superjob}
+    location = '4'
+    keywords_location_search = '1'
+    result_per_page = '100'
+    period_search = '0'
+    
+    vacancies_statistics_list = head_table
 
     for language in languages_list:
-        vacancies_processed = 0
-        salary_language_sum = 0
-
         payload = {
-            'town': 4,
-            'keywords[0][srws]': 1,
+            'town': location,
+            'keywords[0][srws]': keywords_location_search,
             'keywords[0][keys]': language,
-            'count': 100,
-            'period': 0,
+            'count': result_per_page,
+            'period': period_search,
             }
         response = requests.get(url_api_superjob, headers=headers,
                                 params=payload)
         vacancies = response.json()
-        if vacancies['objects'] == 0:
-            vacancies_statistics_superjob_list.append([language, 0, 0,
-                    0])
+        
+        vacancies_all_pages = vacancies['objects']
+        
+        if len(vacancies_all_pages) == 0:
+            vacancies_statistics_list.append([language, 0, 0, 0])
         else:
+            vacancies_statistics_list.append(get_language_statistics(language, source, vacancies_all_pages))
+            
+    table_for_print = get_table_for_print(source, vacancies_statistics_list)
+    return table_for_print
 
-            for vacancy in vacancies['objects']:
-                predict_salary = \
-                    predict_rub_salary_for_superjob(vacancy['payment_from'
-                        ], vacancy['payment_to'])
-                if predict_salary != None:
-                    vacancies_processed += 1
-                    salary_language_sum += predict_salary
-
-        vacancies_found = len(vacancies['objects'])
-
-        try:
-            vacancies_statistics_superjob_list.append([language,
-                    vacancies_found, vacancies_processed,
-                    int(salary_language_sum / vacancies_processed)])
-        except ZeroDivisionError:
-
-            vacancies_statistics_superjob_list.append([language,
-                    vacancies_found, 0, 0])
-
-    return vacancies_statistics_superjob_list
-
-
-def get_table_with_statistics(vacancies_list, title_table):
-    table_for_print = AsciiTable(vacancies_list, title_table)
-    return table_for_print.table
-
-def main():
-    load_dotenv()
+if __name__ == '__main__':
     languages_list =[
           'Java',
           'Python',
@@ -150,19 +153,5 @@ def main():
          'Вакансий\nобработано',
          'Средняя\nзарплата'
          ]]
-
-    title_table_data_headhunter = 'Headhunter Moscow'
-    title_table_data_superjob = 'SuperJob Moscow'
-
-    headhunter_table = \
-        get_table_with_statistics(get_vacancies_statistics_headhunter(languages_list,
-             head_table), title_table_data_headhunter)
-    superjob_table = \
-        get_table_with_statistics(get_vacancies_statistics_superjob(languages_list,
-             head_table), title_table_data_superjob)
-    print(headhunter_table)
-    print(superjob_table)
-    
-
-if __name__ == '__main__':
-    main()
+    print(get_vacancies_statistics_Headhunter('Headhunter', languages_list, head_table))
+    print(get_vacancies_statistics_SuperJob('SuperJob', languages_list, head_table))
